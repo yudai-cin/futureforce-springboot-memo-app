1e0b6173738ba8e6f9abfd6ccd0c10b28923b189
User <User@DESKTOP-8CE140A>
2026-04-04 23:03:34 +0900

    全文検索機能の実装

--- src/main/java/com/lesson/memo/repository/MemoRepository.java
+++ src/main/java/com/lesson/memo/repository/MemoRepository.java
@@ -1,9 +1,11 @@
 package com.lesson.memo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lesson.memo.model.Memo;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-    
+    List<Memo> findByTitleContaining(String keyword);
 }
