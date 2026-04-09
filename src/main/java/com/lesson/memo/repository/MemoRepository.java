36df007d4c2d6178b9130f0397718c7258ad3be3
User <User@DESKTOP-8CE140A>
2026-04-09 21:06:31 +0900

    findByTitleContainingの削除

--- src/main/java/com/lesson/memo/repository/MemoRepository.java
+++ src/main/java/com/lesson/memo/repository/MemoRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.lesson.memo.model.Memo;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-    List<Memo> findByTitleContainingOrContentContaining(String title, String content);
+    List<Memo> findByTitleContainingOrContentContaining(String title, String content); //←findByTitleContainingの削除しました。
 }
